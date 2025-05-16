95fabcfa4be281fa789b1795299b3a7116dfde96
dejan.jovelic <dejan.jovelic@gmail.com>
2025-05-16 18:32:16 +0200

    minimizovan kod stranice moje knjige

--- library/app/mojeknjige/mojeKnjige.js
+++ library/app/mojeknjige/mojeKnjige.js
@@ -1,1 +1,14 @@
 'use strict'
+class Book {
+    constructor(id, name, printDate, url, description, popularity) {
+        this.id = id;
+        this.name = name;
+        this.printDate = printDate;
+        this.url = url;
+        this.description = description;
+        this.popularity = popularity;
+    }
+}
+
+let rented = [];
+let available = [];
